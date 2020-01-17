4ccb078468b2f9c2f06abfd05ff29999d7a734a8
Silent_Coder <36187018+Silentxknight@users.noreply.github.com>
2020-01-18 00:17:21 +0530

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -1,30 +1,31 @@
-// $(function () { 
-// var Pokeurl="https://pokeapi.co/api/v2/pokemon";
+$(function () { 
+var Pokeurl="http://api.open-notify.org/iss-now.json?callback=CALLBACK";
 
-// console.log(Pokeurl);
 
-//  $.getJSON(Pokeurl).done(function(data) {
-//         console.log(data);
+console.log(Pokeurl)
+
+ $.getJSON(Pokeurl).done(function(data) {
+        console.log(data);
       
-//             });
+            });
 
 
 
 
 
-//  });
-var xhr =new XMLHttpRequest();
-xhr.open("GET","http://api.open-notify.org/iss-now.json");
-xhr.onload=function(){
-    if(xhr.readyState!=4 || xhr.status!=200) return;
-    var data=JSON.parse(xhr.responseText);
-   console.log(data);
-   console.log(data['iss_position']);
-   for(i=0;i<data['iss_position'];i++){
+  });
+// var xhr =new XMLHttpRequest();
+// xhr.open("GET","http://api.open-notify.org/iss-now.json?callback=CALLBACK");
+// xhr.onload=function(){
+//     if(xhr.readyState!=4 || xhr.status!=200) return;
+//     var data=JSON.parse(xhr.responseText);
+//    console.log(data);
+//    console.log(data['iss_position']);
+//    for(i=0;i<data['iss_position'];i++){
        
-   }
+//    }
    
    
   
-}
-xhr.send();  
+// }
+// xhr.send();  
